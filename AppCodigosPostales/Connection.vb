e22ef8ed1c986a358055c0672594bf63c04d6b9c
Ruben Riojas <rriojas@gmail.com>
2024-02-20 19:34:40 -0600

    Add project files.

---
+++ AppCodigosPostales/Connection.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Connection
+  Shared cnx As New SqlConnection
+  Private Shared Sub Connect()
+    Try
+      cnx.ConnectionString = "Data Source=localhost\SQLEXPRESS; Initial Catalog=Correos;Integrated Security=True"
+      cnx.Open()
+    Catch ex As Exception
+      MsgBox("Error al conectar a la base de datos: " & ex.Message)
+    End Try
+  End Sub
+  Private Shared Sub Disconect()
+    Try
+      If cnx.State = ConnectionState.Open Then
+        cnx.Close()
+      End If
+    Catch ex As Exception
+      MsgBox("Error al desconectar de la base de datos: " & ex.Message)
+    End Try
+  End Sub
+  'Create a method to excecute a Selection query
+  Public Shared Function SelectQuery(ByVal query As String) As DataTable
+    Dim dt As New DataTable
+    Try
+      Connect()
+      Dim cmd As New SqlCommand(query, cnx)
+      Dim da As New SqlDataAdapter(cmd)
+      da.Fill(dt)
+    Catch ex As Exception
+      MsgBox("Error al ejecutar la consulta: " & ex.Message)
+    Finally
+      Disconect()
+    End Try
+    Return dt
+  End Function
+
+End Class
---
+++ AppCodigosPostales/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+  'Create a constructor
+  Public Sub New()
+    ' This call is required by the designer.
+    InitializeComponent()
+    ' Add any initialization after the InitializeComponent() call.
+    'Create a new instance of the Connection class
+    Dim query As String = "select nombre from estado"
+    DataGridView1.DataSource = Connection.SelectQuery(query)
+  End Sub
+End Class
