23a9e7c69f1e087433d771f330468d3f35f2cc2e
Ruben Riojas <rriojas@gmail.com>
2024-02-27 19:29:03 -0600

    Se agregan filtros por estado y municipio

--- AppCodigosPostales/Form1.Designer.vb
+++ AppCodigosPostales/Form1.Designer.vb
@@ -22,23 +22,64 @@ Partial Class Form1
   'Do not modify it using the code editor.
   <System.Diagnostics.DebuggerStepThrough()>
   Private Sub InitializeComponent()
+    Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+    Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
     DataGridView1 = New DataGridView()
+    cboEstado = New ComboBox()
+    cboMunicipio = New ComboBox()
     CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
     SuspendLayout()
     ' 
     ' DataGridView1
     ' 
-    DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-    DataGridView1.Location = New Point(104, 105)
+    DataGridView1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+    DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft
+    DataGridViewCellStyle1.BackColor = SystemColors.Control
+    DataGridViewCellStyle1.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+    DataGridViewCellStyle1.ForeColor = SystemColors.WindowText
+    DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+    DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+    DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
+    DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+    DataGridView1.ColumnHeadersHeight = 50
+    DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft
+    DataGridViewCellStyle2.BackColor = SystemColors.Window
+    DataGridViewCellStyle2.Font = New Font("Segoe UI", 20F)
+    DataGridViewCellStyle2.ForeColor = SystemColors.ControlText
+    DataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight
+    DataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText
+    DataGridViewCellStyle2.WrapMode = DataGridViewTriState.False
+    DataGridView1.DefaultCellStyle = DataGridViewCellStyle2
+    DataGridView1.Location = New Point(12, 100)
     DataGridView1.Name = "DataGridView1"
-    DataGridView1.Size = New Size(489, 254)
+    DataGridView1.Size = New Size(776, 338)
     DataGridView1.TabIndex = 0
+    ' 
+    ' cboEstado
+    ' 
+    cboEstado.Font = New Font("Segoe UI", 16F)
+    cboEstado.FormattingEnabled = True
+    cboEstado.Location = New Point(12, 26)
+    cboEstado.Name = "cboEstado"
+    cboEstado.Size = New Size(255, 38)
+    cboEstado.TabIndex = 1
+    ' 
+    ' cboMunicipio
+    ' 
+    cboMunicipio.Font = New Font("Segoe UI", 16F)
+    cboMunicipio.FormattingEnabled = True
+    cboMunicipio.Location = New Point(288, 26)
+    cboMunicipio.Name = "cboMunicipio"
+    cboMunicipio.Size = New Size(255, 38)
+    cboMunicipio.TabIndex = 2
     ' 
     ' Form1
     ' 
     AutoScaleDimensions = New SizeF(7F, 15F)
     AutoScaleMode = AutoScaleMode.Font
     ClientSize = New Size(800, 450)
+    Controls.Add(cboMunicipio)
+    Controls.Add(cboEstado)
     Controls.Add(DataGridView1)
     Name = "Form1"
     Text = "Form1"
@@ -47,5 +88,7 @@ Partial Class Form1
   End Sub
 
   Friend WithEvents DataGridView1 As DataGridView
+  Friend WithEvents cboEstado As ComboBox
+  Friend WithEvents cboMunicipio As ComboBox
 
 End Class
--- AppCodigosPostales/Form1.vb
+++ AppCodigosPostales/Form1.vb
@@ -1,11 +1,32 @@
 ﻿Public Class Form1
+  Dim query As String
   'Create a constructor
   Public Sub New()
     ' This call is required by the designer.
     InitializeComponent()
+    DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells
     ' Add any initialization after the InitializeComponent() call.
     'Create a new instance of the Connection class
-    Dim query As String = "select nombre from estado"
+
+    query = "select id, nombre from estado"
+    cboEstado.DataSource = Connection.SelectQuery(query)
+    cboEstado.DisplayMember = "nombre"
+    cboEstado.ValueMember = "id"
+
+  End Sub
+
+  Private Sub cboEstado_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboEstado.SelectedIndexChanged
+    query = "select id, nombre from municipio where idEstado=" & cboEstado.SelectedValue
+    cboMunicipio.DataSource = Connection.SelectQuery(query)
+    cboMunicipio.DisplayMember = "nombre"
+    cboMunicipio.ValueMember = "id"
+  End Sub
+
+  Private Sub cboMunicipio_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboMunicipio.SelectedIndexChanged
+    If cboEstado.Text Is Nothing Then
+      Exit Sub
+    End If
+    query = "select * from VW_Busqueda where [Nombre Municipio]='" & cboMunicipio.Text & "'"
     DataGridView1.DataSource = Connection.SelectQuery(query)
   End Sub
 End Class
